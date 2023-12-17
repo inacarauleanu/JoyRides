b750bf18ea405c4500a6ccc53a3d9d85c2a65b61
ina.carauleanu <ina.carauleanu@gmail.com>
2023-12-17 17:54:09 +0200

    IX-4 <autentificare prin email si parola - v1>

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3LMjJnEN2iUejpL6JUTf-16Dbse6zbD8",
+  authDomain: "joyride-de26f.firebaseapp.com",
+  projectId: "joyride-de26f",
+  storageBucket: "joyride-de26f.appspot.com",
+  messagingSenderId: "634976697024",
+  appId: "1:634976697024:web:d3502a789ab6fd80588b25"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
--- screens/Register.js
+++ screens/Register.js
@@ -1,15 +1,16 @@
 import React, { useCallback, useReducer, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {Colors, Sizes, Fonts} from "../constants/styles.js"
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
-import Input from '../components/input.js';
+import {Input} from 'react-native-elements';
 import Button from '../components/Button.js';
 import { validateInput } from '../utils/actions/formAction.js';
 import { reducer } from '../utils/reducers/formReducers.js';
 import GoogleButton from '../components/GoogleButton.js';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from "C:/Users/inaca/JoyRides/firebase-config.js";
 
 const isTestMode = true;
 
@@ -37,7 +38,47 @@ const initialState = {
 const Register = ({navigation}) =>{
     const [isLoading, setIsLoading] = useState(false);
     const [formState, dispatchFormState] = useReducer(reducer, initialState);
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [fullname, setFullname] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [validationMessage, setValidationMessage] = useState('')
+    const [validationMessageGood, setValidationMessageGood] = useState('')
+  
+    let validateAndSet = (value,setValue) => {
+        setValue(value)
+     }
 
+     function checkPassword(firstpassword,secondpassword) {
+        if(firstpassword !== secondpassword){
+          setValidationMessage('Parolele nu se potrivesc') 
+        }
+        else setValidationMessage('')
+      }
+      async function createAccount() {
+        setValidationMessage('');
+        setValidationMessageGood('');
+        
+         ok = 0;
+
+        email === '' || password === '' 
+        ? setValidationMessage('Toate campurile trebuiesc completate')
+        : ''
+        try {
+          await createUserWithEmailAndPassword(auth, email, password);
+          /*const user = userCredential.user;
+
+        // Update the user's profile with fullname
+            user.updateProfile({
+          displayName: fullname,
+         })*/
+          //navigation.navigate('Landing Page');
+          setValidationMessageGood('Utilizatorul a fost inregistrat cu succes')
+          console.log("bagat");
+        } catch (error) {
+          setValidationMessage(error.message);
+        }
+      }
     const inputChangedHandler = useCallback((inputId, inputValue) =>{
         const result = validateInput(inputId, inputValue)
         dispatchFormState({inputId, validationResult: result, inputValue})
@@ -53,51 +94,59 @@ const Register = ({navigation}) =>{
     <View style = {styles.footer}>
         <KeyboardAwareScrollView>
           {/* <Text style={styles.inputText}>Nume de utilizator...</Text>*/}  
-            <Input 
-                id="fullName"
+            <TextInput style={styles.inputContainer}
+                //id="fullName"
+                value={fullname}
                 placeholder="John Doe"
-                onInputChanged={inputChangedHandler}
+               // onInputChanged={inputChangedHandler}
+               onChangeText={(text) => setFullname(text)}
                 errorText={formState.inputValidities["fullName"]}
-                placeholderTextColor={Colors.black}
+                placeholderTextColor={Colors.myLightGrey}
             />
             {/*<Text style={styles.inputText}>Adresa de e-mail...</Text>*/}
-            <Input 
-                id="email"
+            <TextInput style={styles.inputContainer}
+                //id="email"
+                value={email}
                 placeholder="example@gmail.com"
-                onInputChanged={inputChangedHandler}
+                onChangeText={(text) => setEmail(text)}
                 errorText={formState.inputValidities["email"]}
-                placeholderTextColor={Colors.black}
+                placeholderTextColor={Colors.myLightGrey}
                 keyboardType="email-address"
             />
             {/*<Text style={styles.inputText}>Parola...</Text>*/}
-            <Input 
-                id="password"
+            <TextInput style={styles.inputContainer}
+               // id="password"
+               value={password}
                 placeholder="********"
-                onInputChanged={inputChangedHandler}
+                onChangeText={(value) => validateAndSet(value, setPassword)}
                 errorText={formState.inputValidities["password"]}
-                placeholderTextColor={Colors.black}
+                placeholderTextColor={Colors.myLightGrey}
                 secureTextEntry = {true}
                 autoCapitalize = "none"
             />
            {/* <Text style={styles.inputText}>Confirmă Parola...</Text>*/}
-            <Input 
-                id="confirmPassword"
+            <TextInput style={styles.inputContainer}
+               // id="confirmPassword"
+               value = {confirmPassword}
                 placeholder="********"
-                onInputChanged={inputChangedHandler}
+                onChangeText={(value) => validateAndSet(value,setConfirmPassword)}
                 errorText={formState.inputValidities["confirmPassword"]}
-                placeholderTextColor={Colors.black}
+                placeholderTextColor={Colors.myLightGrey}
                 secureTextEntry = {true}
                 autoCapitalize = "none"
+                onBlur={()=>checkPassword(password,confirmPassword)}
             />
 
             <View style = {styles.helpingText}>
+                    <Text style={styles.error}>{validationMessage}</Text>
+                    <Text style={styles.good}>{validationMessageGood}</Text>
                     <Text style={styles.inputText}>Ai deja un cont? Apasă <Text onPress = {()=>navigation.navigate("Login")} style={styles.helpingTextBold}>aici.</Text></Text> 
                 </View>
 
             <Button
                 title="Autentificare"
                 isLoading={isLoading}
-                onPress = {()=>navigation.navigate("LandingPage")}
+                onPress = {createAccount}
             />
 
             <GoogleButton
@@ -134,6 +183,14 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         paddingBottom: 4
     },
+    error: {
+        marginTop: 10,
+        color: 'red',
+      },
+      good: {
+        marginTop: 10,
+        color: 'green',
+      },
 
     footer: {
         flex: 3,
@@ -152,6 +209,20 @@ const styles = StyleSheet.create({
         ...Fonts.inputText,
         color: Colors.greyForText,
         marginVertical: 4
+    },
+    inputContainer: {
+        width: "100%",
+        backgroundColor: Colors.white,
+        paddingHorizontal: Sizes.padding,
+        paddingVertical: Sizes.padding2,
+        borderRadius: 50,
+        borderWidth: .7,
+        marginVertical: 5,
+        flexDirection: "row",
+        color: Colors.greyForText,
+        flex: 1,
+        fontFamily: "regular",
+   
     },
 
 })
